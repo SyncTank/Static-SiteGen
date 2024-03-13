baaf0e7fc72e0fd609391939eaa84cbda5e58329
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-03-13 00:39:20 -0400

    TextNode to HTMLNode setup

--- src/textnode.py
+++ src/textnode.py
@@ -4,6 +4,14 @@ class TextNode:
         self.text = text
         self.text_Type = text_Type
         self.url = url
+        self.text_type_dict = {
+            "text": "",
+            "bold": "b",
+            "italic": "i",
+            "code": "code",
+            "link": "a",
+            "image": "img"
+        }
 
     def __eq__(self, other):
         return (
@@ -12,5 +20,15 @@ class TextNode:
             self.url == other.url
         )
 
+    def text_node_to_html_node(self):
+        if self.text_Type not in self.text_type_dict:
+            raise Exception(f"Text type not supported: {self.text_Type}")
+        else:
+            pass
+
     def __repr__(self) -> str:
         return f"TextNode( {self.text}, {self.text_Type}, {self.url} )"
+
+
+node = TextNode("Hello World", 'bold', url="")
+node.text_node_to_html_node()
