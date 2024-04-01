95b70fc1d8dca52b56eae8ffbc21a9a982c70263
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-04-01 11:47:38 -0400

    main folder setup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from src.htmlnode import LeafNode
 from textnode import TextNode
+from os import path, mkdir, listdir
 
+def dir_copy():
+    pass
 
 def main() -> None:
     text_node = TextNode('This is a text node', 'link', 'https://www.boot.dev')
