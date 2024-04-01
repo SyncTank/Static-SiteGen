6358e21a65667784ce79002e8f5072e27ed39ca3
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-04-01 12:56:33 -0400

    main folder setup

--- src/main.py
+++ src/main.py
@@ -1,13 +1,44 @@
+import os.path
+import shutil
+
 from src.htmlnode import LeafNode
 from textnode import TextNode
 from os import path, mkdir, listdir
 
-def dir_copy():
+
+def delete_files(path_delete: str) -> None:
     pass
 
-def main() -> None:
-    text_node = TextNode('This is a text node', 'link', 'https://www.boot.dev')
-    print(text_node)
 
+def check_dir(dir_path: str) -> bool:
+    dir_state = False
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    else:
+        dir_state = True
+        if dir_path == "../static":
+            if os.path.exists(f'../backup'):
+                shutil.rmtree(f'../backup')
+            else:
+                os.mkdir(f'../backup')
+
+            dir_copy(dir_path, f"../backup")
+
+    return dir_state
+
+
+def dir_copy(dir_copy_path: str, dir_moveto: str) -> None:
+    items = os.listdir(dir_copy_path)
+    for item in items:
+        print(item)
+        if os.path.isdir(f"{dir_copy_path}/{item}"):
+            os.mkdir(f"{dir_moveto}/{item}")
+            dir_copy(f"{dir_copy_path}/{item}", f"{dir_moveto}/{item}")
+        else:
+            shutil.copy(item, f"{dir_moveto}")
+
+
+def main() -> None:
+    check_dir("../static")
 
 main()
